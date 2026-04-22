3408e5a8fea89d7ac91585758a1de512235fbe57
Robert Smallshire <rob@sixty-north.com>
2026-04-22 21:16:39 +0200

    Bump version: 0.2.1 → 0.2.2

--- src/asyoulikeit/__init__.py
+++ src/asyoulikeit/__init__.py
@@ -1,6 +1,6 @@
 """Utilities for enriching CLI tools with structured report output."""
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from asyoulikeit.cli import ALL_REPORTS, tabulated_output
 from asyoulikeit.exceptions import AsyoulikeitError
